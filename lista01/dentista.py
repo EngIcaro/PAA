3efb5de622f999edd1282930a15a10e068048597
EngIcaro <igpb@ic.ufa.br>
2019-10-31 19:00:55 -0300

    ai

--- lista01/dentista.py
+++ lista01/dentista.py
@@ -17,11 +17,11 @@ for i in range(n):
     consultas.append(aux)
     max = 0
 for j in range(n):
-    melhor = -1
+    melhor = 10000000
     index = 0
     for i in range(n):
-         aux = ((consultas[i][1] - consultas[i][0])/consultas[i][0]) 
-         if(aux > melhor):
+         aux = abs((consultas[i][1]*(consultas[i][0]-consultas[i][1]))) 
+         if(aux < melhor):
              livre = 1
              for k in visitados[consultas[i][0]:consultas[i][1]]:
                  if(k == 1):
@@ -36,3 +36,23 @@ for j in range(n):
         visitados[inicio+i] = 1
     #print(visitados[0:20])
 print(max)
+#%%
+n = int(input(""))
+consultas = []
+visitados = [0]*1000000
+for i in range(n):
+    aux = input("").replace(" ", ",").split(",")
+    aux = [int(x) for x in aux]
+    consultas.append(aux)
+#print(consultas)
+consultas.sort(key=lambda consultas: consultas[1])
+#print(consultas)
+max = 1
+j = 0
+for i in range(n-1):
+    print(consultas[i+1][0], consultas[j][1])
+    if(consultas[i+1][0] >= consultas[j][1]):
+        j = i
+        max +=1
+print(max)
+
